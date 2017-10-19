3c6bd4b3ffd2488a0f9b45212c0146e065f04d9a
chan-smith <chan1019@outlook.com>
2017-10-19 23:37:57 +0800

    ok

---
+++ FirstPractice/Controllers/CategoriesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstPractice.Models;
+using System.Data.Entity;
+
+namespace FirstPractice.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        CategoriesEntities db = new CategoriesEntities();
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View(db.Categories);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CategoryForm categoryForm)
+        {
+            Category category = new Category();
+            category.CategoryName = categoryForm.CategoryName;
+            category.Description = categoryForm.Description;
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Delete(int id=0)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Update(int id=0)
+        {
+            Category category = db.Categories.Find(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Update(CategoryForm categoryForm)
+        {
+            Category category = new Category();
+            category.CategoryID = categoryForm.CategoryID;
+            category.CategoryName = categoryForm.CategoryName;
+            category.Description = categoryForm.Description;
+            db.Entry(category).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FirstPractice/Models/Partial/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FirstPractice.Models
+{
+    [MetadataType(typeof(CategoryMetadata))]
+    public partial class Category
+    {
+        public class CategoryMetadata
+        {
+            public int CategoryID { get; set; }
+
+            [Display(Name ="分類名稱")]
+            [Required(ErrorMessage ="不可空白")]
+            public string CategoryName { get; set; }
+
+            [Display(Name = "分類描述")]
+            [Required(ErrorMessage = "不可空白")]
+            public string Description { get; set; }
+
+            public byte[] Picture { get; set; }
+        }
+
+    }
+}
